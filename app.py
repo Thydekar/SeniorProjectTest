108ffb505b1dfe6ec033a61000b83a8c68291a00
Thaidakar <130446887+Thydekar@users.noreply.github.com>
2025-12-01 20:56:03 -0800

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ import io
 # EDIT ONLY THESE
 NGROK_URL = "https://ona-overcritical-extrinsically.ngrok-free.dev"
 
-MODEL_ASSIGNMENT_GEN = "spartan-assignment"
+MODEL_ASSIGNMENT_GEN = "gemma3"
 MODEL_GRADER         = "spartan-grader"
 MODEL_PLAGIARISM     = "spartan-detector"
 MODEL_STUDENT_CHAT   = "spartan-student"
